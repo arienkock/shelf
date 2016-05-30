6f434f72f54fd46de73bedc8199f5959d609297e
ArienKock <arien.kock@gmail.com>
2016-05-30 13:48:54 +0200

    changed default GC and mem settings, and the way ownership of dirs is done.

--- install-jar.sh
+++ install-jar.sh
@@ -67,7 +67,7 @@ else
 fi
 
 [ -z "$JVM_ARGS" ] && \
-JVM_ARGS="-server -Xms500m -Xmx1g -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -Dsun.net.inetaddr.ttl=60 -Dsun.net.client.defaultConnectTimeout=5000 -Dsun.net.client.defaultReadTimeout=5000"
+JVM_ARGS="-server -Xms64m -Xmx512m -XX:+UseG1GC -XX:MaxMetaspaceSize=64m -Dsun.net.inetaddr.ttl=60 -Dsun.net.client.defaultConnectTimeout=5000 -Dsun.net.client.defaultReadTimeout=5000"
 
 # Default app service user
 [ -z "$APP_USER" ] && APP_USER="$APP_ID"
@@ -85,6 +85,7 @@ get_file()
     echo "File $1 does not exist"
     exit 1
   fi
+  chown "$APP_USER:$APP_USER" "$2"
 }
 
 #                                   888                                                    
@@ -108,13 +109,13 @@ id -u $APP_USER &>/dev/null || useradd -s /sbin/nologin -d "$APP_DIR" "$APP_USER
 # Y88b 888 888 888    Y8b.     Y88b.    Y88b. Y88..88P 888     888 Y8b.          X88 
 #  "Y88888 888 888     "Y8888   "Y8888P  "Y888 "Y88P"  888     888  "Y8888   88888P' 
 
-# Create app directory structure
+# Create app directory structure with proper ownership
 mkdir -p "$APP_DIR"
 if ! sudo -u "$APP_USER" [ -x $APP_DIR ]; then
   echo "Directory $APP_DIR is not accessible to user $APP_USER. Check the parent directory permissions."
   exit 1
 fi
-(cd "$APP_DIR" && mkdir -p bin lib config logs)
+(cd "$APP_DIR" && mkdir -p bin lib config logs && chown "$APP_USER:$APP_USER" ./ bin/ lib/ config/ logs/)
 
 #                                          .d888 d8b 888                   
 #                                         d88P"  Y8P 888                   
@@ -170,20 +171,7 @@ java %%JVM_ARGS%% -jar "lib/%%APP_ID%%.jar" %%APP_ARGS%% &
 wait
 EOF
 
-#                                         d8b                   d8b                            
-#                                         Y8P                   Y8P                            
-#                                                                                              
-# 88888b.   .d88b.  888d888 88888b.d88b.  888 .d8888b  .d8888b  888  .d88b.  88888b.  .d8888b  
-# 888 "88b d8P  Y8b 888P"   888 "888 "88b 888 88K      88K      888 d88""88b 888 "88b 88K      
-# 888  888 88888888 888     888  888  888 888 "Y8888b. "Y8888b. 888 888  888 888  888 "Y8888b. 
-# 888 d88P Y8b.     888     888  888  888 888      X88      X88 888 Y88..88P 888  888      X88 
-# 88888P"   "Y8888  888     888  888  888 888  88888P'  88888P' 888  "Y88P"  888  888  88888P' 
-# 888                                                                                          
-# 888                                                                                          
-# 888
-
 # Make start script executable
-(cd "$APP_DIR" ; chown -R "$APP_USER:$APP_USER"  bin lib config logs)
 chmod 770 "$APP_DIR/bin/start.sh"
 
 # d8b                   888             888 888                                         d8b                  
